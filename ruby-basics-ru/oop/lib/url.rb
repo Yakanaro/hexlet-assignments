a3a5e8a3c9ea1af3d45072eb71c42dc911c89cf5
@hexlet/cli <support@hexlet.io>
2022-11-18 15:13:24 +0300

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'uri'
+require 'facets'
+
+# BEGIN
+class Url
+  include Comparable
+
+  def <=>(other)
+    @url <=> other
+  end
+
+  def initialize(url)
+    @url = url
+  end
+
+  def host
+    uri = URI(@url)
+    uri.host
+  end
+
+  def scheme
+    uri = URI(@url)
+    uri.scheme
+  end
+
+  def query_params
+    uri = URI(@url)
+    uri_query = uri.query
+    result = {}
+    if uri_query.nil?
+      {}
+    else
+      ary = Hash[URI.decode_www_form(uri_query)]
+      result = ary.symbolize_keys
+    end
+    result
+  end
+
+  def query_param(keys, value = nil)
+    new_url = Url.new(@url)
+    obj = new_url.query_params
+    return obj[keys] if obj.key?(keys)
+
+    value
+  end
+end
+
+# END
