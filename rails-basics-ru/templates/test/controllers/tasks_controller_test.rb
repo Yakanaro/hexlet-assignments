cfe1432df43542664798ca14b0e04147564dee5e
@hexlet/cli <support@hexlet.io>
2022-11-25 00:58:47 +0300

    submit rails-basics-ru/templates

--- rails-basics-ru/templates/test/controllers/tasks_controller_test.rb
+++ rails-basics-ru/templates/test/controllers/tasks_controller_test.rb
@@ -1,62 +1,42 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @task = tasks(:one)
-
-    @attrs = {
-      name: Faker::Artist.name,
-      description: Faker::Movies::HarryPotter.quote,
-      status: Faker::Movies::HarryPotter.spell,
-      creator: Faker::Movies::HarryPotter.character,
-      performer: Faker::Movies::HarryPotter.character,
-      completed: Faker::Boolean.boolean
-    }
-  end
-
-  test 'should get index' do
-    get tasks_url
+  test 'opens all tasks page' do
+    get tasks_path
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_task_url
+  test 'opens one task page' do
+    task = Task.create(name: 'test', description: 'test', creator: 'test')
+    get task_path(task.id)
     assert_response :success
   end
 
-  test 'should create task' do
-    post tasks_url, params: { task: @attrs }
+  test 'create task' do
+    post tasks_url, params: { task: { name: 'test', description: 'test', creator: 'test' } }
 
-    task = Task.find_by @attrs
+    task = Task.find_by name: 'test', description: 'test', creator: 'test'
 
     assert { task }
     assert_redirected_to task_url(task)
   end
 
-  test 'should show task' do
-    get task_url(@task)
-    assert_response :success
+  test 'update task' do
+    task = tasks(:one)
+    patch task_url(task), params: { task: { name: 'test', description: 'test', creator: 'test' } }
+
+    task.reload
+
+    assert { task.name == 'test' }
+    assert_redirected_to task_url(task)
   end
 
-  test 'should get edit' do
-    get edit_task_url(@task)
-    assert_response :success
-  end
-
-  test 'should update task' do
-    patch task_url(@task), params: { task: @attrs }
-
-    @task.reload
-
-    assert { @task.name == @attrs[:name] }
-    assert_redirected_to task_url(@task)
-  end
-
-  test 'should destroy task' do
-    delete task_url(@task)
+  test 'destroy task' do
+    task = tasks(:one)
+    delete task_url(task)
 
     assert_redirected_to tasks_url
 
-    assert { !Task.exists? @task.id }
+    assert { !Task.exists? task.id }
   end
 end
