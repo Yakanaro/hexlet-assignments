5b2aa71912da93358cd89b16884533508a7340bc
@hexlet/cli <support@hexlet.io>
2022-12-22 13:42:34 +0300

    submit rails-real-ru/fsm

--- rails-real-ru/fsm/test/controllers/vacancies_controller_test.rb
+++ rails-real-ru/fsm/test/controllers/vacancies_controller_test.rb
@@ -3,41 +3,15 @@
 require 'test_helper'
 
 class VacanciesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @attrs = {
-      title: Faker::Job.title,
-      description: Faker::Lorem.sentence
-    }
-  end
-
-  test 'should get index' do
-    get vacancies_url
-    assert_response :success
-  end
-
-  test 'should get new' do
-    get new_vacancy_url
-    assert_response :success
-  end
-
-  test 'should create vacancy' do
-    post vacancies_url, params: { vacancy: @attrs }
-
-    vacancy = Vacancy.find_by(@attrs)
-
-    assert { vacancy }
-    assert_redirected_to vacancies_url
-  end
-
   test 'publish on moderate' do
     vacancy = vacancies(:on_moderate)
 
     patch publish_vacancy_path(vacancy)
 
-    vacancy.reload
+    assert_response :redirect
 
+    vacancy.reload
     assert { vacancy.published? }
-    assert_redirected_to vacancies_url
   end
 
   test 'archive on moderate' do
@@ -45,10 +19,11 @@ class VacanciesControllerTest < ActionDispatch::IntegrationTest
 
     patch archive_vacancy_path(vacancy)
 
+    assert_response :redirect
+
     vacancy.reload
 
     assert { vacancy.archived? }
-    assert_redirected_to vacancies_url
   end
 
   test 'archive published' do
@@ -56,20 +31,10 @@ class VacanciesControllerTest < ActionDispatch::IntegrationTest
 
     patch archive_vacancy_path(vacancy)
 
-    vacancy.reload
-
-    assert { vacancy.archived? }
-    assert_redirected_to vacancies_url
-  end
-
-  test 'refute publish archived' do
-    vacancy = vacancies(:archived)
-
-    patch publish_vacancy_path(vacancy)
+    assert_response :redirect
 
     vacancy.reload
 
     assert { vacancy.archived? }
-    assert_redirected_to vacancies_url
   end
 end
