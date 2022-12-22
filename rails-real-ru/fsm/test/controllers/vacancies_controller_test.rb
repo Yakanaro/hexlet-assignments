0deebb573eee78a83ecbf8e1b8199f675fa314e2
@hexlet/cli <support@hexlet.io>
2022-12-22 13:45:04 +0300

    submit rails-real-ru/fsm

--- rails-real-ru/fsm/test/controllers/vacancies_controller_test.rb
+++ rails-real-ru/fsm/test/controllers/vacancies_controller_test.rb
@@ -3,38 +3,4 @@
 require 'test_helper'
 
 class VacanciesControllerTest < ActionDispatch::IntegrationTest
-  test 'publish on moderate' do
-    vacancy = vacancies(:on_moderate)
-
-    patch publish_vacancy_path(vacancy)
-
-    assert_response :redirect
-
-    vacancy.reload
-    assert { vacancy.published? }
-  end
-
-  test 'archive on moderate' do
-    vacancy = vacancies(:on_moderate)
-
-    patch archive_vacancy_path(vacancy)
-
-    assert_response :redirect
-
-    vacancy.reload
-
-    assert { vacancy.archived? }
-  end
-
-  test 'archive published' do
-    vacancy = vacancies(:published)
-
-    patch archive_vacancy_path(vacancy)
-
-    assert_response :redirect
-
-    vacancy.reload
-
-    assert { vacancy.archived? }
-  end
 end
